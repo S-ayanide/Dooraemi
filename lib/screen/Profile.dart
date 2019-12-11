197d3788d4a023151b377eed1a2486b4503c3ce9
Sayan Mondal <sayanmondal342@gmail.com>
2019-12-11 16:37:34 +0800

    Profile Changes

--- lib/screen/Profile.dart
+++ lib/screen/Profile.dart
@@ -19,13 +19,41 @@ class _ProfileState extends State<Profile> {
         body: Column(
           children: <Widget>[
             Container(
-              height: 450.0,
-              width: 500.0,
+              height: 430.0,
+              width: MediaQuery.of(context).size.width,
               child: Image(
                 image: NetworkImage(widget.pic),
                 fit: BoxFit.cover,
               )
             ),
+            Container(
+              child: Center(
+                child: Text(widget.value, style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0
+                  ),  
+                ),
+              ),
+              color: Colors.black,
+              height: 55.0,
+              width: MediaQuery.of(context).size.width,              
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Icon(Icons.mic, size: 60.0,),
+                  onPressed: (){},
+                  color: Color(0xFFaef879),
+                  padding: const EdgeInsets.fromLTRB(70.0,15.0,75.0,20.0),
+                ),
+                RaisedButton(
+                  child: Icon(Icons.message, size: 60.0,),
+                  onPressed: (){},
+                  color: Color(0xFF8acaf6),
+                  padding: const EdgeInsets.fromLTRB(75.0,15.0,70.0,20.0),
+                ),
+              ],
+            )
           ],
         ),
       ),
